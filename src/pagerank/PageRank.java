0c7c02a64e5e264a58b999e21ee0400b64224093
Gerardo Figueroa <gerardo_ofc@yahoo.com>
2016-04-26 16:04:30 +0800

    Changed some docs

--- src/pagerank/PageRank.java
+++ src/pagerank/PageRank.java
@@ -25,7 +25,11 @@ public class PageRank {
     protected long start_time = 0L;
     protected long elapsed_time = 0L;
 
-    // Public constructor (for use in other classes)
+    /**
+     * Public constructor (for use in other classes)
+     * @param pageRankProperties
+     * @throws Exception 
+     */
     public PageRank(PageRankProperties pageRankProperties) 
             throws Exception {
         this.pageRankProperties = pageRankProperties;
@@ -57,7 +61,21 @@ public class PageRank {
     }
 
     /**
-     * Main entry point.
+     * Main entry point for PageRank.
+     * PageRank first reads a graph file.
+     * The Graph Matrix File (must be in adjacency matrix format
+     * with the node labels only on the top row (no labels on first column).
+     * The rows after the first row contain the edge weights (1 for unweighted).
+     * The separator between labels and edge weights is specified in the properties
+     * file.
+     * Example (separator is space):
+     * label1 label2 label3 ...
+     * weight_n1n1 weight_n1n2 weight_n1n3 ...
+     * weight_n2n1 weight_n2n2 weight n2n3 ...
+     * ...
+     * @param graphFileDir
+     * @return an array of Node sorted by ranking
+     * @throws Exception 
      */
     public Node[] run(String graphFileDir) throws Exception {
 
